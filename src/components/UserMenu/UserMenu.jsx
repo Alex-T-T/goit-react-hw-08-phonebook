abdfa3509fee5bf2e162a77039c754ecba50ed68
Olexandr Tolochko <0504fusion@gmail.com>
2022-11-10 15:44:57 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,16 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import userAuthSelectors from "redux/userAuth/userAuth-selectors";
 import css from "../UserMenu/UserMenu.module.css";
 import userAuthOperations from "redux/userAuth/userAuth-operations";
-import { useNavigate } from 'react-router-dom';
 
 export const UserMenu = () => {
     const dispatch = useDispatch();
-    const navigate = useNavigate();
     const email = useSelector(userAuthSelectors.getUserEmail);
 
     const logout = () => {
         dispatch(userAuthOperations.userLogout());
-        navigate("/login");
     }
 
     return (
